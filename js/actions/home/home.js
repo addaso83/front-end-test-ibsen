ead98bf1b2d14ac3f545611da6f4741251c86bb8
Ibsen Perez <iperez@people-inmotion.com>
2020-12-28 00:31:42 -0500

    Se agrego jdatatable, filtro con busqueda y paginacion.

--- js/actions/home/home.js
+++ js/actions/home/home.js
@@ -1,11 +1,57 @@
-
-
-
 $(function(){
+    loadCategories();
+    loadTBL1();
 
-    axios.get("http://localhost:8000/api/categorias"
-    ).then(function (res) {
+    $("#btnSearch").on("click", function(ev){
+        ev.preventDefault();
+        loadTBL1();
+    });
+
+    $("#btnClean").on("click", function(ev){
+        ev.preventDefault();
+        loadCategories();
+        $("#txtTitle").val('');
+        $("#txDescription").val('')
+        loadTBL1();
+    });
     
+});
+
+function loadTBL1(){
+    $("#tbl_1").DataTable({
+        "responsive": true,
+        "autoWidth": false,
+        "stateSave": false,
+        "processing": true,
+        "serverSide": true,
+        "destroy": true,
+        searching: false,
+        paging: true,
+        "lengthMenu": [[25, 30,40], [25,30, 40]],
+        "ajax": {
+            "url": "http://localhost:8000/api/articulospublicados",
+            "type": "POST",
+            "async": true,
+            "data" : {categoria : $("#lstCategory").val(), titulo : $("#txtTitle").val(), descripcion:$("#txDescription").val()}
+        },
+        "columns": [
+            { "data": "titulo" },
+            { "data": "descripcion_corta" },
+            { "data": "publicador" },
+            { "data": "created_at" },
+            { "data": "id" }
+        ],
+        "rowCallback": function (row, data, index) {
+            $('td:eq(4)', row).html(
+                "<a class='btn btn-info btn-sm'  href='/" + data.id + "'  title='Ver Anuncio'><i class='far fa-eye'></i></a>" 
+            );
+        }
+    });
+}
+
+function loadCategories(){
+    axios.get("http://localhost:8000/api/categorias"
+    ).then(function (res) {   
     var categorias = res.data;
     $("#lstCategory").empty();
     $("#lstCategory").append('<option value=' + ''  +'>'+'Seleccionar'+'</option>');
@@ -16,5 +62,4 @@ $(function(){
     }).catch(function (err) {
     
     });
-
-});
+}
